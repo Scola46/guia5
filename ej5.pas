f0bc4825b38b36fa4f67560aac40407bf7838dfa
Scola46 <112493848+Scola46@users.noreply.github.com>
2022-10-20 20:36:36 -0300

    Add files via upload

--- ej5.pas
+++ ej5.pas
@@ -1,100 +1,132 @@
 program ej5;
+{5.- Leer de un archivo de texto sobre tres arreglos los datos de un conjunto de autos, por cada uno:
+- Patente
+- Año
+- Precio
+Se pide mediante un menú que permita la repetición de las opciones con diferentes valores,
+calcular:
+a. Para un año dado, precio mínimo (puede no existir)
+b. Para un precio dado cantidad de vehículos por debajo de dicho valor
+c. Para un rango de años dado [Año1, Año2] precio promedio de los autos en dicho rango (puede
+no existir)
+7.- A partir de ejercicio 5 y para una patente dada, mostrar los datos del auto (indicar si no se
+encontró).}
 type
-  TS6=string[6];
-  tV1=array [1..50] of ts6;
-  tv2=array [1..50] of word;
-  tv3=array [1..50] of real;
-procedure leervec(var v1:tv1;var v2:tv2;var v3:tv3;var N:byte);
+  st6=string[6];
+  V1=array[1..50]of string[6];
+  V2=array[1..50]of word;
+  v3=array[1..50]of real;
+procedure leervec(var vpat:v1;var va:v2;var vpre:v3;var n:byte);
 var
   arch:text;
 begin
   assign (arch,'autos.txt');
   reset (arch);
   n:=0;
-  while not eof (arch) do
-        begin
-          n:=n+1;
-          readln (arch,v1[n],v2[n],v3[n]);
+  while not eof(arch)do
+        BEGIN
+        n:=n+1;
+        readln (arch,vpat[n],va[n],vpre[n]);
         end;
-  close (arch);
+
+ close (arch);
 end;
-function Ppreciomin(v3:tv3;n:byte;v2:tv2;A:word):real;
+function premin (va:v2;vpre:v3;n:byte;anio:word):real;
 var
   i:byte;
   min:real;
-  begin
-    min:=999999;
-    PPreciomin:=0;
-    for i:=1 to n do
-        if (v2[i]=A)and(v3[i]<min)then
-           begin
-           min:=v3[i];
-           ppreciomin:=I;
-           end;
-  end;
-function contveh(v3:tv3;N:byte;p:real):byte;
-var
-  i,cont:byte;
 begin
-  cont:=0;
-  for i:=1 to N do
-      if v3[i]<P then
-         cont:=cont+1;
-      contveh:=cont;
+  min:=999999;premin:=0;
+  for i:=1 to n do
+      begin
+        If (va[i]=anio)and(vpre[i]<min)then
+           begin
+             min:=vpre[i];
+             premin:=(vpre[i])
+           end
+      end;
+
 end;
-function Precioprom (v2:tv2;v3:tv3;n:byte;a1,a2:word):real;
+function preciovh (vpre:v3;pre:real;dim2:byte):byte;
 var
-  i,cont:byte;
+  i,contvh:byte;
+begin
+  contvh:=0;
+  for i:=1 to dim2 do
+      begin
+        if (vpre[i]<pre) then
+           begin
+             writeln ('entro');
+             contvh:=contvh+1;
+             preciovh:=contvh;
+           end;
+      end;
+  writeln (contvh,'cont');
+end;
+function preprom(anio1,anio2:word;vpre:v3;va:v2;dim3:byte):real;
+var
+  i,conta:byte;
   total:real;
 begin
-  cont:=0;total:=0;
-  for i:=1 to n do
-      if (v2[i]>=a1)and(v2[i]<=a2) then
-         begin
-         cont:=cont+1;
-         total:=total+v3[i];
-         end;
-      if cont<>0 then
-         precioprom:=total/cont
-      else
-         precioprom:=0;
-end;
-var
-  pat:tv1;
-  anio:tv2;
-  precio:tv3;
-  A,A1,a2:word;
-  P,prom:real;
-  n,pos,cont:byte;
-begin
-  repeat
-  writeln ('Ingrese un año');readln(A);
-  until (a>0)and(A<=2022);
-  repeat
-  writeln('Ingrese un precio');readln(P);
-  until p>0  ;
-  repeat
-  writeln('Ingrese un año');readln(A1);
-  until (a1>0)and(a1<=2022);
-  repeat
-  writeln('Ingrese un año');readln(A2);
-  until (a2>1)and(a2<2022);
-  leervec(pat,anio,N);
-  pos:=ppreciomin(precio,n,anio,a);
-  if pos<>0 then
-     writeln ('El preco minimo es ',precio[pos]);
-  else
-      writeln ('No hay autos en ese año');
-  cont:=contveh(precio,n,p);
-  if cont<>0 then
-     writeln('La cantidad de vehiculos es ',cont);
-  else
-      writeln ('No hay vehiculos debajo de ese precio');
-  precioprom(anio,precio,n,a1,a2);
-  if prom<>0 then
-     writeln('el precio promedio es ',prom:8:2);
-  else
-      writeln ('No hay autos en ese rango');
+  conta:=0;total:=0;
+  for i:=1 to dim3 do
+      begin
+        if ((va[i]>anio1)and(va[i]<anio2))or((va[i]>anio2)and(va[i]<anio1))then
+           begin
+             conta:=conta+1;
+             total:=total+vpre[i];
+             preprom:=total/conta
+           end;
 
+      end;
+end;
+procedure patente (vpat:v1;va:v2;vpre:v3;paten:st6;dim4:byte;var pre2:real;var va2:word;var contp:byte);
+var
+  i:byte;
+begin
+  contp:=0;
+  FOR I:=1 to dim4 do
+      if vpat[i]=paten then
+         begin
+           contp:=contp+1;
+           va2:=va[i];
+           pre2:=vpre[i];
+         end;
+end;
+
+var
+  n,cont,i,contp:byte;
+  VPAT:v1;
+  Va:v2;
+  vpre,vmin:v3;
+  anio,a1,a2,va2:word;
+  precio,pre2:real;
+  paten:st6;
+
+begin
+ repeat
+   writeln ('Ingresar un anio');readln(anio);
+ until (anio>0) ;
+ repeat
+   writeln ('Ingrese un precio');readln(precio);
+ until (precio>0);
+ repeat
+   writeln ('Ingrese anio 1');readln (a1);
+ until (a1>0);
+ repeat
+   writeln ('Ingrese anio2');readln(a2);
+ until (a2>0);
+   writeln ('Ingrese patente');readln (paten);
+ leervec (vpat,va,vpre,n);
+ IF premin(va,vpre,n,anio)<>0 THEN
+ writeln ('precio minimo ', premin(va,vpre,n,anio):8:2)
+ else
+   Writeln ('No existe');
+ writeln ();
+ writeln ('cantidad de precios por debajo del valor ',preciovh(vpre,precio,n));
+ writeln ('Precio promedio ',preprom(a1,a2,vpre,va,n):8:2);
+ patente (vpat,va,vpre,paten,n,pre2,va2,contp);
+ writeln ('Datos de la patente ingresada. Anio ',va2,' Precio ',pre2:8:2);
+ readln;
 end.
 
